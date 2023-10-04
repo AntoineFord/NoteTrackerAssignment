f39fa65dd1b0ba2df1f54b52e6901ff3103211c3
Antoine <130304994+AntoineFord@users.noreply.github.com>
2023-10-03 20:36:35 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 3001;
+const notesdb = require('./db/db.json');
+const id = require('uuid').v4;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => res.json(notesdb));
+
+app.post('/api/notes', (req, res) => {
+  const newNoteTitle = req.body.title;
+  const newNoteText = req.body.text;
+
+  const newNote = { 
+    title: newNoteTitle, 
+    text: newNoteText
+  };
+
+  // Assign new note an id
+  newNote.id = id();
+  notesdb.push(newNote);
+  res.json(newNote);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  const note = notesdb.find(note => note.id === id);
+  const index = notesdb.indexOf(note);
+  notesdb.splice(index, 1);
+  res.json(notesdb);
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
